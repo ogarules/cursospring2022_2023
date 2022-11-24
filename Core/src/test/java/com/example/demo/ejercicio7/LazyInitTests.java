46165a070545f8c9e9400bb3516d10a222d9de0a
Oscar Garcia <ogarcia@upexternos.com.mx>
2022-11-23 21:01:11 -0600

    Se agrega primera parte del ejercicio 7

---
+++ Core/src/test/java/com/example/demo/ejercicio7/LazyInitTests.java
@@ -0,0 +1,22 @@
+package com.example.demo.ejercicio7;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LazyInitTests {
+    private static ApplicationContext context;
+
+    @BeforeClass
+    public static void beforeClass() {
+        context = new ClassPathXmlApplicationContext("spring/ejercicio7/beans.xml");
+    }
+
+    @Test
+    public void lazyInitTest() {
+    }
+}

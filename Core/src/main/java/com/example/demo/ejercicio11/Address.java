7169e30897b6b3fcdecb2d7fd05ad0c30e5649c5
Oscar Garcia <ogarcia@upexternos.com.mx>
2022-11-30 20:31:10 -0600

    avance ejercicio11

---
+++ Core/src/main/java/com/example/demo/ejercicio11/Address.java
@@ -0,0 +1,13 @@
+package com.example.demo.ejercicio11;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Address {
+    private String street;
+    private String externalNumber;
+    private String internalNumber;
+    private String neighbor;
+}
---
+++ Core/src/main/java/com/example/demo/ejercicio11/Person.java
@@ -0,0 +1,12 @@
+package com.example.demo.ejercicio11;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+    private String name;
+    private Address address;
+    private int age;
+}

24e16bbfcfb64df411d3320e57fe49357d3d7cd1
Oscar Garcia <ogarcia@upexternos.com.mx>
2022-12-07 20:35:24 -0600

    Avance ejercicio 15

---
+++ Core/src/main/java/com/example/demo/ejercicio15/setter/Journalist.java
@@ -0,0 +1,41 @@
+package com.example.demo.ejercicio15.setter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+public class Journalist {
+    private String name;
+    private Integer age;
+    private String rfc;
+
+    private Notebook notebook;
+    private Pen pen;
+
+    @Autowired
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Autowired
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Autowired
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    @Autowired
+    public void setNotebook(Notebook notebook) {
+        this.notebook = notebook;
+    }
+
+    @Autowired
+    public void setPen(Pen pen) {
+        this.pen = pen;
+    }    
+}

7e59aa934685943bc0ab73f484d8df418bd5f4fb
Oscar <oscar@digitalchallengers.com.mx>
2023-02-01 22:01:40 -0600

    ejercicio27

---
+++ ORM/src/main/java/com/example/demo/ejercicio27/dao/impl/GenericHibernateDAO.java
@@ -0,0 +1,59 @@
+package com.example.demo.ejercicio27.dao.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.ejercicio27.dao.IGenericDAO;
+
+import lombok.Getter;
+
+public abstract class GenericHibernateDAO<T, ID extends Serializable> implements IGenericDAO<T,ID> {
+    
+    protected @Getter final Class<T> persistentClass;
+
+    @Autowired
+    protected @Getter SessionFactory sessionFactory;
+
+    public GenericHibernateDAO(Class<T> type){
+        this.persistentClass = type;
+    }
+
+    @Override
+    public T delete(ID id) {
+        T entity = this.findById(id);
+        return this.delete(entity);
+    }
+
+    @Override
+    public T delete(T entity) {
+        this.sessionFactory.getCurrentSession().delete(entity);
+        return entity;
+    }
+
+    @Override
+    public List<T> findAll() {
+        return (List<T>)sessionFactory.getCurrentSession().createQuery("FROM " + this.persistentClass.getName())
+               .list();
+    }
+
+    @Override
+    public T findById(ID id) {
+        return this.sessionFactory.getCurrentSession().get(this.persistentClass, id);
+    }
+
+    @Override
+    public void insert(T entity) {
+        this.sessionFactory.getCurrentSession().save(entity);
+        
+    }
+
+    @Override
+    public void update(T entity) {
+        this.sessionFactory.getCurrentSession().update(entity);
+    }
+
+    
+}
---
+++ ORM/src/main/java/com/example/demo/ejercicio27/domain/Account.java
@@ -0,0 +1,44 @@
+package com.example.demo.ejercicio27.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@Entity
+@Table(name = "SPRING_DATA_ACCOUNT_TBL")
+@ToString(exclude = "customer")
+@EqualsAndHashCode(exclude ="customer")
+public class Account {
+    
+    @Id
+    @Column(name="ACCOUNT_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="ACCOUNT_NUMBER")
+    private String accountNumber;
+
+    @Column(name="CREATED_DATE")
+    private Date createdDate;
+
+    @Column(name="BALANCE")
+    private BigDecimal balance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="FK_CUSTOMER_ID")
+    private Customer customer;
+}

ab3af39ad34377491c21cd5fc8cc73bb1843be21
Oscar <oscar@digitalchallengers.com.mx>
2023-02-01 20:31:50 -0600

    jdbc transaction annotation

--- JDBC/src/main/java/com/example/demo/ejercicio26/service/TransactionalService.java
+++ JDBC/src/main/java/com/example/demo/ejercicio26/service/TransactionalService.java
@@ -1,6 +1,9 @@
 package com.example.demo.ejercicio26.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.ejercicio26.domain.BusinessObject;
 
@@ -8,8 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
 public class TransactionalService implements ITransactionalService {
 
+    @Transactional(readOnly = true)
     @Override
     public BusinessObject getBusinessObject(Long id) {
         log.info("Getting business object");
---
+++ JDBC/src/test/java/com/example/demo/ejercicio26/AnnotationTransactionTests.java
@@ -0,0 +1,51 @@
+package com.example.demo.ejercicio26;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.demo.ejercicio26.domain.BusinessObject;
+import com.example.demo.ejercicio26.service.ITransactionalService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= AppConfig.class)
+public class AnnotationTransactionTests {
+    @Autowired
+    ITransactionalService servce;
+
+    @Test
+    public void transactionalServiceTest(){
+        log.info("Testing transactional configuration");
+
+        BusinessObject bo = null;
+
+        bo = servce.getBusinessObject(1L);
+
+        log.info("Inserting business object");
+
+        try{
+            servce.insertBusinessObject(bo);
+        }
+        catch(Exception e){
+            log.info("Insert error {}", e.getMessage());
+        }
+
+        log.info("Updating business object");
+        
+        try{
+            servce.updateBusinessObject(bo);
+        }
+        catch(Exception e){
+            log.info("Update error {}", e.getMessage());
+        }
+
+        log.info("Deleting business object...");
+
+        servce.deleteBusinessObject(1L);
+    }
+}

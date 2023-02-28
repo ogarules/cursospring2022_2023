463a3048d2172173bc1447597f332b5c4bb4148b
Oscar <oscar@digitalchallengers.com.mx>
2023-02-27 20:37:41 -0600

    Seagreg swagger en codigo

--- SpringBoot/Desing/demo/src/main/java/com/example/demo/controllers/PetController.java
+++ SpringBoot/Desing/demo/src/main/java/com/example/demo/controllers/PetController.java
@@ -14,10 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.Pet;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags={"Pet api"}, description="API to manage the pet shop")
 @RestController
 @RequestMapping("/pets")
 public class PetController {
     
+    @ApiOperation(value="Get all pets", produces="application/json", consumes="application/json", protocols="http,https", notes="For getting all available pets in store")
+    @ApiResponses(value={
+        @ApiResponse(code = 200, message="Success"),
+        @ApiResponse(code = 404, message="No pets found in store")
+    })
     @GetMapping
     public List<Pet> getAllPets(){
 
@@ -25,7 +36,7 @@ public class PetController {
         Pet pet = new Pet();
         pets.add(pet);
         pet.setId(1L);
-        pet.setName("Fido");
+        pet.setName("F  ido");
         pet.setTag("ABC-123");
         pet.setSpecies("Dog");
 

4665b4486baf69309de9704b9c99eb61f11b597b
Oscar <oscar@digitalchallengers.com.mx>
2023-02-27 20:44:44 -0600

    Add api model properties to pet

--- SpringBoot/Desing/demo/src/main/java/com/example/demo/models/Pet.java
+++ SpringBoot/Desing/demo/src/main/java/com/example/demo/models/Pet.java
@@ -1,12 +1,21 @@
 package com.example.demo.models;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+@ApiModel(description = "Pet information")
 @Data
 public class Pet {
     
+    @ApiModelProperty(notes = "Internal id", example= "123")
     private Long id;
+    @ApiModelProperty(notes = "Pet's name", example= "fido")
     private String name;
+
+    @ApiModelProperty(notes = "Pet's etag", example= "ABC-123")
     private String tag;
+
+    @ApiModelProperty(notes = "Pet's species", example= "Dog")
     private String species;
 }
